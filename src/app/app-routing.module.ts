4a310b926244641cba65e149556751575614d325
Abhi Sharma <sharm395@purdue.edu>
2023-05-29 18:47:22 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./TabContent/home/home.component";
+import {EducatorsComponent} from "./TabContent/educators/educators.component";
+import {PricesComponent} from "./TabContent/prices/prices.component";
+import {CoursesComponent} from "./TabContent/courses/courses.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'educators', component: EducatorsComponent },
+  { path: 'pricing', component: PricesComponent},
+  { path: 'courses', component: CoursesComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {MatTabsModule} from "@angular/material/tabs";
+import { MatButtonModule } from '@angular/material/button';
+import { HomeComponent } from './TabContent/home/home.component';
+import { CoursesComponent } from './TabContent/courses/courses.component';
+import { EducatorsComponent } from './TabContent/educators/educators.component';
+import { PricesComponent } from './TabContent/prices/prices.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    CoursesComponent,
+    EducatorsComponent,
+    PricesComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatTabsModule,
+        MatButtonModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
